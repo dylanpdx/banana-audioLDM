61b421f2de64edc0651981d7e98c9dc95a06b963
Dylan <dylanpdx@gmail.com>
2023-02-25 18:00:54 +0000

    Intial commit

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,12 @@ FROM pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime
 WORKDIR /
 
 # Install git
-RUN apt-get update && apt-get install -y git
+RUN apt-get update && apt-get install -y git libsndfile1
 
 # Install python packages
-RUN pip3 install --upgrade pip
-ADD requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
+RUN pip3 install audioldm
+RUN pip3 install numpy==1.20.3
+RUN pip3 install sanic==22.6.2
 
 # We add the banana boilerplate here
 ADD server.py .
--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from transformers import pipeline
+from audioldm import text_to_audio, build_model, save_wave
+import soundfile as sf
+import os
 import torch
+import base64
+from random import randint
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
@@ -7,20 +11,44 @@ def init():
     global model
     
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    model = build_model()
 
+
+def text2audio(text, duration, guidance_scale, random_seed, n_candidates):
+    # print(text, length, guidance_scale)
+    waveform = text_to_audio(
+        model,
+        text,
+        seed=random_seed,
+        duration=duration,
+        guidance_scale=guidance_scale,
+        n_candidate_gen_per_text=int(n_candidates),
+    )
+    return waveform
+        
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
-
+    
+    mode=model_inputs.get('mode', "text2audio")
+    duration=model_inputs.get('duration', 5)
+    guidance_scale=model_inputs.get('guidance_scale', 2.5)
+    random_seed=model_inputs.get('random_seed', randint(0,100000))
+    n_candidates=model_inputs.get('n_candidates', 3)
+    
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
     if prompt == None:
         return {'message': "No prompt provided"}
     
     # Run the model
-    result = model(prompt)
-
+    result = text2audio(prompt,duration,guidance_scale,random_seed,n_candidates)
+    save_wave(result,"/tmp","gen.wav")
+    in_file = open("/tmp/gen_0.wav", "rb")
+    data = in_file.read()
+    in_file.close()
+    os.remove("/tmp/gen_0.wav")
+    b64_text = base64.b64encode(data).decode("ascii")
     # Return the results as a dictionary
-    return result
+    return b64_text
